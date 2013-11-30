1cf4bf3b758fc7b26d7b84af6a3c8f596b0e5f04
oznetmaster <ncolvin@nivloc.com>
2013-11-30 12:00:54 +0800

    ! Unsupported commit message encoding: fatal: ambiguous argument 'HEAD': unknown revision or path not in the working tree. !
    Use '--' to separate paths from revisions, like this:
    'git <command> [<revision>...] -- [<file>...]'

---
+++ SSMonoGenericCollectionsLibrary/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle ("SSMonoGenericCollectionsLibrary")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("SSMonoGenericCollectionsLibrary")]
+[assembly: AssemblyCopyright ("Copyright ©  2013")]
+[assembly: AssemblyVersion ("1.0.0.*")]
+
