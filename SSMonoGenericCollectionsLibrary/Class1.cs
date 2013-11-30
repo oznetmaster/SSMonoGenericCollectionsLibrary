03cc606664231c512d84a7021de80e43090eba24
oznetmaster <ncolvin@marvelous.com>
2013-11-30 12:00:54 +0800

    ! Unsupported commit message encoding: fatal: ambiguous argument 'HEAD': unknown revision or path not in the working tree. !
    Use '--' to separate paths from revisions, like this:
    'git <command> [<revision>...] -- [<file>...]'

---
+++ SSMonoGenericCollectionsLibrary/Class1.cs
@@ -0,0 +1,25 @@
+﻿
+namespace MySpace
+	{
+	public class Class1<TKey, TValue>
+		{
+		class Node
+			{
+			public TKey key;
+			public TValue value;
+
+			public Node (TKey key, TValue value)
+				{
+				this.key = key;
+				this.value = value;
+				}
+			}
+
+		private Node a;
+
+		public Class1 (TKey key, TValue value)
+			{
+			a = new Node (key, value);
+			}
+		}
+	}
